92ddf439a0216b8e589faa41b6be1e3e60556c20
Mariia Postovik <accaunt01.13@gmail.com~>
2023-10-25 23:57:52 +0300

    new update

--- testimonials.js
+++ testimonials.js
@@ -9,20 +9,65 @@ import './fonts/stylesheet.css'
 import './style.css'
 import './responsive.css'
 
-let modal     = document.getElementById( "myModal" );
-let orderBtn1 = document.getElementById( "orderBtn1" );
-let span      = document.getElementsByClassName( "close" )[0];
- 
-orderBtn1.onclick = function() {
-  modal.style.display = "block";
+const MODAL_ACTIVE_CLASS_NAME = 'modal-active';
+
+const formModal = document.querySelector('#form-modal');
+const successModal = document.querySelector('#success-modal');
+const form = document.querySelector('#form');
+
+const openFormModalBtn = document.querySelector('#open-form-modal-btn');
+const launchBtn = document.querySelector('#launch-btn');
+const closeBtns = document.querySelectorAll('.close-btn');
+
+openFormModalBtn.addEventListener('click', () => {
+    formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+})
+
+const closeFormModal = () => {
+    formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+};
+
+const closeSuccessModal = () => {
+    successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+};
+
+const openSuccessModal = () => {
+    successModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+};
+
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        e.stopPropagation();
+        closeFormModal();
+        closeSuccessModal();
+    })
+})
+
+function clearFormFields() {
+    const modalFields = formModal.querySelectorAll('input');
+
+    modalFields.forEach( field => { 
+        field.value = ''
+    });
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(form);
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => {
+
+        setTimeout(() => {
+            closeFormModal();
+            setTimeout(openSuccessModal, 700);
+            setTimeout(closeSuccessModal, 300);
+            clearFormFields();
+        }, 300);
+      })
+      .catch((error) => console.log('Sending form failed'));
+})
